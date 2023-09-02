f8ec1a30964bb5a9481347f94122b28ce96d47b3
yashviordex123 <125557869+yashviordex123@users.noreply.github.com>
2023-09-02 13:21:18 +0530

    ad

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://yashvi:9428291198@cluster0.bjtxavf.mongodb.net/Ecommerce');
+    const conn = await mongoose.connect('mongodb+srv://yashvi:9428291198@cluster0.bjtxavf.mongodb.net/Ecomm');
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
